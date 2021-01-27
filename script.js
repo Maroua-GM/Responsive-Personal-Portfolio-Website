98925a2013a0b77115dcf414a4650874e7656be1
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-27 13:29:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if (this.scrollY > 20) {
+			$('.navbar').addClass("sticky");
+		} else {
+            $('.navbar').removeClass("sticky");
+		}
+	});
+});
