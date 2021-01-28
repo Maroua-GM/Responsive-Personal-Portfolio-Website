b29fb73e9b99d6187052baad365577257dfd008d
BENKHEDIR Maroua <maroua2012@gmail.com>
2021-01-28 20:10:57 +0100

    footer section

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ $(document).ready(function () {
 			$('.navbar').addClass("sticky");
 		} else {
 			$('.navbar').removeClass("sticky");
-		}
+        }
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
 	});
+     // slide up script
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop:0})
+     })
 
 	//toggle menu/navbar script
 
@@ -14,6 +23,19 @@ $(document).ready(function () {
         $('.navbar .menu').toggleClass("active");
           $('.menu-btn i').toggleClass("active");
     });
+    //typing animation script
+var typed=new Typed(".typing",{
+    strings:["Developper","Freelancer","Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+var typed=new Typed(".typing2",{
+    strings:["Developper","Freelancer","Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
     //owl carousel script
     $('.carousel').owlCarousel({
         margin:20,
